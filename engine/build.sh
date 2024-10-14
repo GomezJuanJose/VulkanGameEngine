02ba49c41513831f84b49ec23ede2e5632b09f35
JuanJose Gomez <gjuanjo1999@gmail.com>
2024-10-15 01:25:54 +0200

    Inital setup for Windows and Linux

---
+++ engine/build.sh
@@ -0,0 +1,20 @@
+# Build script for engine
+set echo on
+
+mkdir -p ../bin
+
+# Get a list of all .c files
+cFilenames=$(find . -type f -name "*.c")
+
+#echo "Files:" $cFilenames
+
+assembly="engine"
+compilerFlags="-g -shared -fdeclspec -fPIC"
+# -fms-extensions
+# -Wall -Werror
+includeFlags="-Isrc -I$VULKAN_SDK/include"
+linkerFlags="-lvulkan -lxcb -lx11 -lx11-xcb -lxkbcommon -L$VULKAN_SDK/lib -L/usr/X11R6/lib"
+defines="-D_DEBUG -DTEXPORT"
+
+echo "Building $assembly..."
+clang $cFilenames $compilerFlags -o ../bin/lib$assembly.so $defines $includeFlags $linkerFlags
---
+++ engine/src/test.c
@@ -0,0 +1,6 @@
+#include "test.h"
+#include <stdio.h>
+
+void AbrirTaller(){
+    printf("¡Se abre el Taller!");
+}
