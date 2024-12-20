f2319837bcb36f6c532e95076c593fa117323a5a
JuanJose Gomez <gjuanjo1999@gmail.com>
2024-12-20 20:39:09 +0100

    Added some code to the game proyect to print in the console the number of dynamic allocations.

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -22,5 +22,7 @@ b8 create_game(game* out_game){
     // Create the game state.
     out_game->state = tallocate(sizeof(game_state), MEMORY_TAG_GAME);
 
+    out_game->application_state = 0;
+
     return TRUE;
 }
